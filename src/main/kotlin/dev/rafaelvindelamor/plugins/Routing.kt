9e74850626bae79ff216aa3a9f956cdca4a577ac
Rafael Vindel Amor <rafael.vindel@gmail.com>
2022-01-10 19:54:28 +0100

    Add basic Ktor project structure

---
+++ src/main/kotlin/dev/rafaelvindelamor/plugins/Routing.kt
@@ -0,0 +1,27 @@
+package dev.rafaelvindelamor.plugins
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun Application.configureRouting() {
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        install(StatusPages) {
+            exception<AuthenticationException> {
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> {
+                call.respond(HttpStatusCode.Forbidden)
+            }
+        }
+    }
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
