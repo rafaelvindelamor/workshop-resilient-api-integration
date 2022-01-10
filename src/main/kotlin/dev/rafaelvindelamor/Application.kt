0073fc8a5f5944ec6b6c8eb267e161d5ee89f976
Rafael Vindel Amor <rafael.vindel@gmail.com>
2022-01-10 20:45:09 +0100

    Handle optional value for the PORT env variable

--- src/main/kotlin/dev/rafaelvindelamor/Application.kt
+++ src/main/kotlin/dev/rafaelvindelamor/Application.kt
@@ -1,11 +1,16 @@
 package dev.rafaelvindelamor
 
+import arrow.core.Option
+import arrow.core.getOrElse
 import dev.rafaelvindelamor.plugins.configureRouting
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+
+    val port = Option.fromNullable(System.getenv("PORT")).map { value -> Integer.parseInt(value) }.getOrElse { 8080 }
+
+    embeddedServer(Netty, port = port, host = "0.0.0.0") {
         configureRouting()
     }.start(wait = true)
 }
